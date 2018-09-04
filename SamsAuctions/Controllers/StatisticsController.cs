926714cd67396fd5d13441092bf3d0705edd9921
Samuel Bering <samuelbering@hotmail.com>
2018-09-04 21:03:10 +0200

    statistics createviewmodel

--- SamsAuctions/Controllers/StatisticsController.cs
+++ SamsAuctions/Controllers/StatisticsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using SamsAuctions.BL;
 using SamsAuctions.Infrastructure;
 using SamsAuctions.Models;
+using SamsAuctions.Models.ViewModels;
 
 namespace SamsAuctions.Controllers
 {
@@ -25,10 +27,51 @@ namespace SamsAuctions.Controllers
             _userManager = userManager;
         }
 
-        //private async Task<>
-        public async Task<IActionResult> Index()
+        private async Task<AuctionsStatisticsViewModel> CreateAuctionsStatisticsViewModel(IList<Auction> auctions)
+        {
+            var viewModel = new AuctionsStatisticsViewModel();
+
+            auctions = auctions.OrderBy(a => a.SlutDatum).ToList();
+
+            var lastDate = new DateTime(1000, 1, 1);
+
+            foreach (var auction in auctions)
+            {
+                var winningBid = await _auctions.GetWinningBid(auction);
+                if (winningBid == null)
+                    continue;
+
+                viewModel.ReservationPrices.Add(auction.Utropspris);
+                viewModel.FinalPrices.Add(winningBid.Summa);
+                viewModel.Differences.Add(winningBid.Summa - auction.Utropspris);
+
+                if (auction.SlutDatum.Year==lastDate.Year)
+                {
+                    if (auction.SlutDatum.Month == lastDate.Month)
+                    {
+                        viewModel.Points.Add("");
+                    }
+                    else
+                        viewModel.Points.Add(auction.SlutDatum.ToString("MMM", CultureInfo.CreateSpecificCulture("sv-SE")));
+                }
+                else
+                    viewModel.Points.Add(auction.SlutDatum.ToString("yyyy MMM", CultureInfo.CreateSpecificCulture("sv-SE")));
+
+                lastDate = auction.SlutDatum;
+            }
+
+            return viewModel;
+        }
+
+        public async Task<IActionResult> GetAuctionsStatistics()
         {
             var auctions = await _auctions.GetClosedAuctions(groupCode, User);
+            var viewModel = await CreateAuctionsStatisticsViewModel(auctions);
+            return Ok(viewModel);
+        }
+
+        public IActionResult Index()
+        {
             return View();
         }
 
---
+++ SamsAuctions/Models/ViewModels/AuctionsStatisticsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamsAuctions.Models.ViewModels
+{
+    public class AuctionsStatisticsViewModel
+    {
+        public AuctionsStatisticsViewModel()
+        {
+            Points = new List<string>();
+            ReservationPrices = new List<int>();
+            FinalPrices = new List<int>();
+            Differences = new List<int>();
+        }
+
+        public IList<string> Points { get; set; }
+        public IList<int> ReservationPrices { get; set; }
+        public IList<int> FinalPrices { get; set; }
+        public IList<int> Differences { get; set; }
+    }
+}
